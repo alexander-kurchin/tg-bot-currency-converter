19b553aab14993481f32d947a8250fb43db16662
Alexander Kurchin <80466494+alexander-kurchin@users.noreply.github.com>
2023-08-08 15:42:35 +0300

    add command_help

--- main.py
+++ main.py
@@ -16,5 +16,16 @@ def command_start(message):
     bot.send_message(message.chat.id, text)
 
 
+@bot.message_handler(commands=['help'])
+def command_help(message):
+    text = 'Инструкции:\n\n'
+    text += 'Необходимо ввести через пробел:\n\n<A> <B> <C>\n\n'
+    text += 'Где <A> — валюта, из которой конвертируем, '
+    text += '<B> — валюта, в которую конвертируем, '
+    text += '<C> — количество первой валюты.\n\n'
+    text += 'Доступные валюты: /currencies'
+    bot.send_message(message.chat.id, text)
+
+
 if __name__ == '__main__':
     bot.polling(non_stop=True)
