f53ef3a0b9a56b46ec1683e37d163aea1e8f2d71
Alexander Kurchin <80466494+alexander-kurchin@users.noreply.github.com>
2023-08-08 19:14:25 +0300

    Add Docstrings

--- extensions.py
+++ extensions.py
@@ -5,35 +5,62 @@ import requests
 from currencies import CURRENCIES
 
 
-class APIException(Exception):
+class DataValidationException(Exception):
+    """
+    Ошибка валидации данных.
+    """
+
     pass
 
 
 class Converter():
+    """
+    Конвертер валют.
+    """
+
     @staticmethod
-    def get_price(base: str, quote: str, base_amount: str)  -> str:
+    def get_price(base: str, quote: str, base_amount: str) -> str:
+        """
+        Конвертирует одну валюту в другую на основе
+        курса валют ЦБ РФ на сегодня, полученного
+        через API от сервиса https://www.cbr-xml-daily.ru/.
+
+        Args:
+            base (str): валюта, из которой конвертируем
+            quote (str): валюта, в которую конвертируем
+            base_amount (str): количество валюты base
+
+        Raises:
+            APIException: ошибка валидации данных
+
+        Returns:
+            str: ответ в чат с пользователем
+        """
+
         try:
             base_key = CURRENCIES[base.lower()]
         except KeyError:
             e = f'Ошибка! Валюта «{base}» не найдена.'
-            raise APIException(e)
+            raise DataValidationException(e)
         try:
             quote_key = CURRENCIES[quote.lower()]
         except KeyError:
             e = f'Ошибка! Валюта «{quote}» не найдена.'
-            raise APIException(e)
+            raise DataValidationException(e)
         if base_key == quote_key:
             e = f'Ошибка! Невозможно конвертировать одинаковые валюты «{base}».'
-            raise APIException(e)
+            raise DataValidationException(e)
         try:
             base_amount = float(base_amount)
         except ValueError:
             e = f'Ошибка! Не удалось обработать количество «{base_amount}».'
-            raise APIException(e)
+            raise DataValidationException(e)
 
         response = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
         response = json.loads(response.content)
 
+        # Так как в полученном json есть только курсы иностранных валют
+        # по отношению к рублю, существует три варианта расчёта
         if quote_key == 'RUB':
             quote_amount = base_amount * response['Valute'][base_key]['Value']
         elif base_key == 'RUB':
@@ -41,5 +68,6 @@ class Converter():
         else:
             quote_amount = base_amount * response['Valute'][base_key]['Value']
             quote_amount = quote_amount / response['Valute'][quote_key]['Value']
+
         quote_amount = round(quote_amount, 2)
         return f'{base_amount:.2f} {base_key} → {quote_amount} {quote_key}'
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import telebot
 from dotenv import load_dotenv
 
 from currencies import CURRENCIES
-from extensions import APIException, Converter
+from extensions import Converter, DataValidationException
 
 
 load_dotenv()
@@ -14,6 +14,14 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def command_start(message: telebot.types.Message) -> None:
+    """
+    Обработчик команды /start.
+
+    Args:
+        message (telebot.types.Message):
+            объект «Сообщение» из библиотеки pyTelegramBotAPI
+    """
+
     text = f'Привет, @{message.chat.username}!\n'
     text += 'Я бот-конвертер валют.\n'
     text += 'Нажмите /help, если нужны инструкции.'
@@ -22,6 +30,14 @@ def command_start(message: telebot.types.Message) -> None:
 
 @bot.message_handler(commands=['help'])
 def command_help(message: telebot.types.Message) -> None:
+    """
+    Обработчик команды /help.
+
+    Args:
+        message (telebot.types.Message):
+            объект «Сообщение» из библиотеки pyTelegramBotAPI
+    """
+
     text = 'Инструкции.\n\n'
     text += 'Необходимо ввести через пробел:\n\n<A> <B> <C>\n\n'
     text += 'Где <A> — валюта, из которой конвертируем, '
@@ -33,6 +49,14 @@ def command_help(message: telebot.types.Message) -> None:
 
 @bot.message_handler(commands=['currencies', 'values'])
 def command_currencies(message: telebot.types.Message) -> None:
+    """
+    Обработчик команд /currencies и /values.
+
+    Args:
+        message (telebot.types.Message):
+            объект «Сообщение» из библиотеки pyTelegramBotAPI
+    """
+
     text = 'Доступные валюты:\n\n'
     text += '\n'.join([key for key in CURRENCIES.keys()])
     bot.send_message(message.chat.id, text)
@@ -42,19 +66,38 @@ def command_currencies(message: telebot.types.Message) -> None:
                                     'location', 'photo', 'poll', 'sticker',
                                     'video', 'video_note', 'voice'])
 def message_scum(message: telebot.types.Message) -> None:
+    """
+    Обработчик мусорных сообщений.
+
+    Args:
+        message (telebot.types.Message):
+            объект «Сообщение» из библиотеки pyTelegramBotAPI
+    """
+
     text = 'Это очень интересно.'
     bot.reply_to(message, text)
 
 
 @bot.message_handler()
 def message_main(message: telebot.types.Message) -> None:
+    """
+    Обработчик основных сообщений.
+
+    Args:
+        message (telebot.types.Message):
+            объект «Сообщение» из библиотеки pyTelegramBotAPI
+
+    Raises:
+        APIException: ошибка валидации данных
+    """
+
     try:
         values = message.text.strip().split(' ')
         if len(values) != 3:
             e = 'Ошибка! Аргументов должно быть 3 (три). Инструкции: /help'
-            raise APIException(e)
+            raise DataValidationException(e)
         text = Converter.get_price(*values)
-    except APIException as e:
+    except DataValidationException as e:
         text = e
     except Exception as e:
         text = f'Что-то пошло не так:\n\n{e}\n\nПопробуйте зайти позже.'
