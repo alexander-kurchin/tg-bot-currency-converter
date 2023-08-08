45377562e604c0ce7e9d686559e811704f1ee662
Alexander Kurchin <80466494+alexander-kurchin@users.noreply.github.com>
2023-08-08 15:42:04 +0300

    add command_start

--- main.py
+++ main.py
@@ -7,5 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 bot = telebot.TeleBot(os.getenv('TOKEN'))
 
+
+@bot.message_handler(commands=['start'])
+def command_start(message):
+    text = f'Привет, @{message.chat.username}!\n'
+    text += 'Я бот-конвертер валют.\n'
+    text += 'Нажми /help, если нужны инструкции.'
+    bot.send_message(message.chat.id, text)
+
+
 if __name__ == '__main__':
     bot.polling(non_stop=True)
